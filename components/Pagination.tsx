f04a819b9647a1a30e918568ac63d3c4b399faef
keXXpert <kexxpert@ya.ru>
2021-12-25 16:10:04 +0300

    updated pagination prev-next buttons

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,7 +1,6 @@
 import {IPagination} from 'boundless-api-client/types/common';
 import {createGetStr} from 'boundless-api-client/utils.js';
 import clsx from 'clsx';
-import Link from 'next/link';
 import {useRouter} from 'next/router';
 import React from 'react';
 
@@ -27,28 +26,32 @@ export default function Pagination({pagination, params, onChange}: IPaginationPr
 		<nav className={clsx('d-flex justify-content-center', pageCount < 2 && 'd-none')}>
 			<ul className='pagination'>
 				<li className={clsx('page-item', currentPage <= 1 && 'disabled')}>
-					<Link href={getFullNavUrl(prevPageNum)} shallow>
-						<a className='page-link' aria-label='Previous'>
-							<span aria-hidden='true'>&laquo;</span>
-						</a>
-					</Link>
+					<a
+						className='page-link'
+						aria-label='Previous'
+						onClick={onClick.bind(null, prevPageNum)}
+						href={getFullNavUrl(prevPageNum)}
+					>
+						<span aria-hidden='true'>&laquo;</span>
+					</a>
 				</li>
 				{[...Array(pageCount)].map((_, index) => (
 					<li className={clsx('page-item', index + 1 === currentPage && 'active')} key={index}>
-						{/*<Link href={getFullNavUrl(index + 1)} shallow>*/}
 						<a className='page-link'
 							onClick={onClick.bind(null, index + 1)}
 							href={getFullNavUrl(index + 1)}
 						>{index + 1}</a>
-						{/*</Link>*/}
 					</li>
 				))}
 				<li className={clsx('page-item', currentPage >= pageCount && 'disabled')}>
-					<Link href={getFullNavUrl(nextPageNum)} shallow>
-						<a className='page-link' aria-label='Next'>
-							<span aria-hidden='true'>&raquo;</span>
-						</a>
-					</Link>
+					<a
+						className='page-link'
+						aria-label='Previous'
+						onClick={onClick.bind(null, nextPageNum)}
+						href={getFullNavUrl(nextPageNum)}
+					>
+						<span aria-hidden='true'>&raquo;</span>
+					</a>
 				</li>
 			</ul>
 		</nav>
