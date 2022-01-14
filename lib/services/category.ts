86d30727646a490743ba8927ad9a66a38f6ec62d
Kirill Zhirnov <ekvixgroup@gmail.com>
2022-01-14 17:12:52 +0300

    clear unsused code

--- lib/services/category.ts
+++ lib/services/category.ts
@@ -1,20 +1,5 @@
 import {IGetProductsParams} from 'boundless-api-client/endpoints/catalog';
-import {ICategoryFlatItem, ICategoryItem} from 'boundless-api-client/types/catalog/category';
 import {TQuery} from '../../@types/common';
-
-export const getMenu4Category = (category: ICategoryItem): ICategoryFlatItem[] => {
-	const categoryMenu: ICategoryFlatItem[] = [];
-	const {parent_id, siblings, children} = category;
-
-	if (children && children.length > 0) {
-		categoryMenu.push(...children);
-	} else if (siblings && siblings.length > 0) {
-		const filteredSiblings = siblings.filter(elem => elem.parent_id === parent_id);
-		categoryMenu.push(...filteredSiblings);
-	}
-
-	return categoryMenu;
-};
 
 export const filterProductsQuery = (query: TQuery): IGetProductsParams => {
 	const allowedKeys = ['in_stock', 'price_min', 'price_max', 'brand', 'sort', 'props', 'page', 'per-page'];
