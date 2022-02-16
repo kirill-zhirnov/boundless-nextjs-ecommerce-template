9dc004728cd2995713072da36a716c357148c334
keXXpert <kexxpert@ya.ru>
2022-02-16 12:12:20 +0300

    styles naming improvement

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {useAppDispatch} from '../hooks/redux';
 import {setIsOpened} from '../redux/reducers/asideMenu';
 
-export default function Header() {
+export default function Header({companyTitle}: {companyTitle?: string}) {
 	const dispatch = useAppDispatch();
 
 	const onHamburgerBtnClicked = (e: MouseEvent<HTMLButtonElement>) => {
@@ -16,21 +16,23 @@ export default function Header() {
 		dispatch(setIsOpened(true));
 	};
 
+	const title = companyTitle || 'Your Company LLC.';
+
 	return (
-		<header className='page__header'>
-			<div className='container text-center position-relative'>
-				<div className='d-flex justify-content-between py-2 align-items-center'>
-					<div className='page__header-logo'>
+		<header className='page-header'>
+			<div className='container'>
+				<div className='page-header__content'>
+					<div className='page-header__logo'>
 						<Link href='/'>
 							<a>
-								<img src={logoImg.src} width={logoImg.width} height={logoImg.height} alt={'Boundless commerce store'} />
+								<img src={logoImg.src} width={logoImg.width} height={logoImg.height} alt={title} />
 							</a>
 						</Link>
 					</div>
 					<HeaderCart />
 					<button type={'button'}
-									className={'btn btn-outline-secondary'}
-									onClick={onHamburgerBtnClicked}
+						className={'btn btn-outline-secondary'}
+						onClick={onHamburgerBtnClicked}
 					>
 						<FontAwesomeIcon icon={faBars} />
 					</button>
