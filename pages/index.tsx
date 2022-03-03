81aa501f11d18eab742e0c846c4f95cee9290763
Kirill Zhirnov <ekvixgroup@gmail.com>
2022-03-03 15:13:30 +0300

    minor improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import {makeAllMenus} from '../lib/menu';
 import VerticalMenu from '../components/VerticalMenu';
 import {IMenuItem} from '../@types/components';
 import ProductsSlider from '../components/ProductsSlider';
-import SliderWrapper from '../components/SliderWrapper';
+import SwiperSlider from '../components/SwiperSlider';
 
 export default function IndexPage({products, mainMenu, footerMenu}: InferGetServerSidePropsType<typeof getServerSideProps>) {
 	return (
@@ -23,7 +23,7 @@ export default function IndexPage({products, mainMenu, footerMenu}: InferGetServ
 					</div>
 				</div>
 				<ProductsSlider products={products.slice(0, 5)} />
-				<SliderWrapper />
+				<MainPageSlider />
 			</div>
 		</MainLayout>
 	);
@@ -47,4 +47,46 @@ interface IIndexPageProps {
 	products: IProduct[];
 	mainMenu: IMenuItem[];
 	footerMenu: IMenuItem[];
+}
+
+function MainPageSlider() {
+	const slides = [
+		{
+			'img': 'https://i4431-static.my-sellios.ru/media/tpl-images/24/df/6703cb392146512eeb43899ff8cf.jpeg',
+			'link': '',
+			'caption': 'Три вещи нельзя скрыть: солнце, луну и истину.',
+			'captionPosition': 'center',
+			'useFilling': true,
+			'fillingColor': '#000000',
+			'fillingOpacity': 0.40
+		},
+		{
+			'img': 'https://i4431-static.my-sellios.ru/media/tpl-images/a0/af/669bf3da0205acffd5f31d093e38.jpeg',
+			'link': '',
+			'caption': null,
+			'captionPosition': null,
+			'useFilling': false,
+			'fillingColor': '#000000',
+			'fillingOpacity': 0.4
+		},
+		{
+			'img': 'https://i4431-static.my-sellios.ru/media/tpl-images/26/f0/7e3a35271c7bc381dfac2814cbe7.jpg',
+			'link': '',
+			'caption': 'Some text',
+			'captionPosition': 'top',
+			'useFilling': false,
+			'fillingColor': '#000000',
+			'fillingOpacity': 0.4
+		}
+	];
+
+	return (
+		<SwiperSlider
+			showPrevNext
+			roundCorners
+			pagination='progressbar'
+			size={'medium'}
+			slides={slides}
+		/>
+	);
 }
